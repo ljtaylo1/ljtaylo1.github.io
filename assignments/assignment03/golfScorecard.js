5fbaf89a1725d738180483fad43e3437f666189d
ljtaylo1 <89933681+ljtaylo1@users.noreply.github.com>
2021-10-18 21:32:06 -0400

    Update golfScorecard.js

--- assignments/assignment03/golfScorecard.js
+++ assignments/assignment03/golfScorecard.js
@@ -1,60 +1,5 @@
 let elem = [];
 
-// assign the entire table row for hole 1 to a variable, elem
-elem[1]
-  = document.getElementById("1");
-
-elem[2]
-  = document.getElementById("2");
-
-elem[3]
-  = document.getElementById("3");
-
-elem[4]
-  = document.getElementById("4");
-
-elem[5]
-  = document.getElementById("5");
-
-elem[6]
-  = document.getElementById("6");
-
-elem[7]
-  = document.getElementById("7");
-
-elem[8]
-  = document.getElementById("8");
-
-elem[9]
-  = document.getElementById("9");
-
-elem[10]
-  = document.getElementById("10");
-
-elem[11]
-  = document.getElementById("11");
-
-elem[12]
-  = document.getElementById("12");
-
-elem[13]
-  = document.getElementById("13");
-
-elem[14]
-  = document.getElementById("14");
-
-elem[15]
-  = document.getElementById("15");
-
-elem[16]
-  = document.getElementById("16");
-
-elem[17]
-  = document.getElementById("17");
-
-elem[18]
-  = document.getElementById("18");
-
 // display the number of children (all td elements)
 //console.log(elem.children.length);
 // display the content of the + button, which is the first child of the fifth element
@@ -71,8 +16,6 @@ for(let i=1; i<=18; i++) {
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
     elem.children[2].innerHTML = "1";
-  else if(elem.children[2].innerHTML == elem.children[1].innerHTML)
-    elem.children[3].innerHTML + 1;
   else {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
