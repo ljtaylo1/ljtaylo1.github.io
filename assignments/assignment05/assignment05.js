3ed2fa457bec77c07242637db0aaf39ab7904b50
ljtaylo1 <89933681+ljtaylo1@users.noreply.github.com>
2021-11-06 10:18:55 -0400

    Update assignment05.js

--- assignments/assignment05/assignment05.js
+++ assignments/assignment05/assignment05.js
@@ -108,7 +108,7 @@ function loadContent() {
         = newConfirmedOver1000.map( 
           (x) => x.NewDeaths );
       chartData.options.title.text 
-        = "Covid 19 Hotspots" ;
+        = "Covid 19 Hotspots As Of " + (dayjs().format());
       myChart = new Chart(ctx, chartData); 
 
     } // end if
